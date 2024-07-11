574d9d52259769d25f9bd2588f112a05aeef3336
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2024-07-11 21:09:24 +0500

    feat: added update post api to swagger

--- dist/routes/post.route.js
+++ dist/routes/post.route.js
@@ -43,5 +43,59 @@ const router = (0, express_1.Router)();
  *        description: Server Error
  */
 router.post("/create", post_validation_1.createPostValidator, post_validation_1.validate, post_controller_1.createPostController);
+/** POST Methods */
+/**
+ * @openapi
+ * '/api/post/update':
+ *  put:
+ *     tags:
+ *     - Post Controller
+ *     summary: Update a Post
+ *     parameters:
+ *       - name: id
+ *         in: query
+ *         description: The id of the post to update
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: body
+ *         name: body
+ *         description: Updated post data
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             title:
+ *               type: string
+ *               default: Updated Title Of The Post
+ *             descriptiom:
+ *               type: string
+ *               default: Updated Description Of The Post
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - title
+ *              - description
+ *            properties:
+ *              title:
+ *                type: string
+ *                default: Title Of The Post
+ *              description:
+ *                type: string
+ *                default: Description Of The Post
+ *     responses:
+ *      200:
+ *         description: Post updated successfully
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: Post Not Found
+ *      500:
+ *        description: Server Error
+ */
 router.put("/update", post_validation_1.updatePostValidator, post_validation_1.validate, post_controller_1.updatePostController);
 exports.default = router;
--- routes/post.route.ts
+++ routes/post.route.ts
@@ -52,6 +52,60 @@ const router = Router();
  */
 router.post("/create", createPostValidator, validate, createPostController);
 
+/** POST Methods */
+/**
+ * @openapi
+ * '/api/post/update':
+ *  put:
+ *     tags:
+ *     - Post Controller
+ *     summary: Update a Post
+ *     parameters:
+ *       - name: id
+ *         in: query
+ *         description: The id of the post to update
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: body
+ *         name: body
+ *         description: Updated post data
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             title:
+ *               type: string
+ *               default: Updated Title Of The Post
+ *             descriptiom:
+ *               type: string
+ *               default: Updated Description Of The Post
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - title
+ *              - description
+ *            properties:
+ *              title:
+ *                type: string
+ *                default: Title Of The Post
+ *              description:
+ *                type: string
+ *                default: Description Of The Post
+ *     responses:
+ *      200:
+ *         description: Post updated successfully
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: Post Not Found
+ *      500:
+ *        description: Server Error
+ */
 router.put("/update", updatePostValidator, validate, updatePostController);
 
 export default router;
